a3ffb67e8e93487feedd68fb4be9455f8f73d936
PremSaiBollamoni <premsai200804@gmail.com>
2026-04-30 15:07:37 +0530

    fix: make email optional in login request

--- src/main/java/com/campusfieldtrack/dto/AuthRequest.java
+++ src/main/java/com/campusfieldtrack/dto/AuthRequest.java
@@ -18,7 +18,6 @@ public class AuthRequest {
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
 
-    @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
     private String email;
 
